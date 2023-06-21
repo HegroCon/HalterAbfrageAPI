647aeda003c61d7d64eed5b9ed48ee064b97fec9
Hay <uidn7289@contiwan.com>
2023-06-21 10:40:01 +0200

    Test

--- HalterAbfrageAPI/Models/Person.cs
+++ HalterAbfrageAPI/Models/Person.cs
@@ -1,12 +1,20 @@
-﻿namespace HalterAbfrageAPI.Models
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HalterAbfrageAPI.Models
 {
     public class Person
     {
         public int Id { get; set; }
+        [Column(TypeName = "varchar(MAX)")]
         public string Name { get; set; }
+        [Column(TypeName = "varchar(MAX)")]
         public string Vorname { get; set; }
+        [Column(TypeName = "varchar(MAX)")]
         public string StrasseHausnummer { get; set; }
+
         public DateTime Birthday { get; set; }
+
         public Stadt Stadt { get; set; }
         public List<Fahrzeug> fahrzeuge;
     }
