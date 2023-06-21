9627281c2f8cc4d6bed3554eb06653f499bcd69f
Heike Gross <uid14452@contiwan.com>
2023-06-21 10:00:49 +0200

    init models

---
+++ HalterAbfrageAPI/Models/Fahrzeug.cs
@@ -0,0 +1,11 @@
+﻿namespace HalterAbfrageAPI.Models
+{
+    public class Fahrzeug
+    {
+        public string Kennzeichen { get; set; }
+        public string Marke { get; set; }
+        public string Kategory { get; set; }
+        public string Farbe { get; set; }
+
+    }
+}
