24edff8a5226ac8e746a482b949a2c17accff3a1
SimonHayConti <simon.hay@continental.com>
2023-06-21 11:55:30 +0200

    PersonController hinzugefügt

---
+++ HalterAbfrageAPI/Controllers/PersonController.cs
@@ -0,0 +1,76 @@
+using HalterAbfrageAPI.Data;
+using HalterAbfrageAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HalterAbfrageAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+        private readonly MyDbContext _context;
+        
+        [HttpGet]
+        public async Task<ActionResult<List<Person>>> GetPersonen()
+        {
+            return Ok(await _context.Personen
+                .Include(f => f.fahrzeuge)
+                .ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<Person>>> GetPersonById(int id)
+        {
+            var person = await _context.Personen
+                .Include(p => p.fahrzeuge)
+                .FirstOrDefaultAsync(p => p.Id == id);
+            if (person == null)
+                return NotFound();
+            return Ok(person);
+        }
+        
+        [HttpPost]
+        public async Task<ActionResult<Person>> CreatePerson(Person person)
+        {
+            if (!await _context.Personen.AnyAsync(p => p.Id == person.Id))
+                return BadRequest("Invalid Person");
+
+            person.fahrzeuge = null;
+
+            _context.Personen.Add(person);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetPersonById), new { id = person.Id }, person);
+        }
+        [HttpPut]
+        public async Task<ActionResult<Person>> UpdatePerson(Person person)
+        {
+            if (!await _context.Personen.AnyAsync(p => p.Id == person.Id))
+                return BadRequest("Invalid Person");
+
+            person.fahrzeuge = null;
+
+            if (await _context.Personen.ContainsAsync(person))
+            {
+                _context.Personen.Update(person);
+                await _context.SaveChangesAsync();
+                return Ok(person);
+            }
+            return BadRequest("Person nicht gefunden");
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Fahrzeug>> DeleteFahrzeug(int id)
+        {
+            var p = await _context.Personen.FindAsync(id);
+
+            if (p == null)
+                return BadRequest("Person nicht gefunden");
+
+            _context.Personen.Remove(p);
+            await _context.SaveChangesAsync();
+
+            return Ok("Person entfernt");
+        }
+    }
+}
