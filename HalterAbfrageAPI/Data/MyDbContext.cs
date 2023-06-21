e28a1823f2ab9320aee2fcda7e2b3fa19b27a06a
Bleicher Anna-Lena (uidw7981) <anna-lena.bleicher@continental-corporation.com>
2023-06-21 11:25:03 +0200

    delete Weatherforecast

--- HalterAbfrageAPI/Data/MyDbContext.cs
+++ HalterAbfrageAPI/Data/MyDbContext.cs
@@ -9,22 +9,7 @@ namespace HalterAbfrageAPI.Data
         public DbSet<Person> Personen { get; set; }
         public DbSet<Fahrzeug> Fahrzeuge { get; set; }
 
-        public MyDbContext(string connectionString) : base(connectionString) { }
-        public MyDbContext() { }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-
-            modelBuilder.Entity<Person>()
-                .HasRequired(e => e.Stadt)
-                .WithMany(c => c.Personen);
-
-            modelBuilder.Entity<Fahrzeug>()
-                .HasRequired(e => e.Person)
-                .WithMany(c => c.fahrzeuge);
-
-            base.OnModelCreating(modelBuilder);
-        }
+        public MyDbContext(DbContextOptions options) : base(options) { }
 
     }
 }
